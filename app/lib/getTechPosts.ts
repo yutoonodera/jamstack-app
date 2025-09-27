12a9f2185184fc1438f0bc09ac963c0439f20f64
onody <onoderayuto@onody00.local>
2025-09-27 23:15:48 +0900

    fix

--- app/lib/getTechPosts.ts
+++ app/lib/getTechPosts.ts
@@ -7,7 +7,7 @@ export type Post = {
 };
 
 export async function getTechPosts(): Promise<Post[]> {
-  const baseUrl = process.env.WORDPRESS_API_URL || "http://localhost:8000";
+  const baseUrl = process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "http://localhost:8000";
   const res = await fetch(
     `${baseUrl}/wp-json/wp/v2/posts?_embed&categories=3`
   );
