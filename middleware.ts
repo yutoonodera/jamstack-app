406c697264843a6c54d87bb2d6bdd215d9778823
onody <onoderayuto@onody00.local>
2025-09-30 19:33:18 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log("ENV:", process.env.NEXT_PUBLIC_ENV);
 
   if (process.env.NEXT_PUBLIC_ENV !== "staging") {
     return NextResponse.next();
   }
 
   const auth = request.headers.get("authorization");
-  console.log("Auth Header:", auth);
 
   const user = process.env.STAGING_AUTH_USER || "staging";
   const pass = process.env.STAGING_AUTH_PASSWORD || "staging";
