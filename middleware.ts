acd4ab2ad2758ae7c494034f737113e73ac45b5c
onody <onoderayuto@onody00.local>
2025-09-30 19:30:07 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,15 @@
-// app/middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  // 環境がstagingでなければスルー
+  console.log("ENV:", process.env.NEXT_PUBLIC_ENV);
+
   if (process.env.NEXT_PUBLIC_ENV !== "staging") {
     return NextResponse.next();
   }
 
   const auth = request.headers.get("authorization");
+  console.log("Auth Header:", auth);
 
   const user = process.env.STAGING_AUTH_USER || "staging";
   const pass = process.env.STAGING_AUTH_PASSWORD || "staging";
@@ -21,5 +22,7 @@ export function middleware(request: NextRequest) {
     });
   }
 
-  return NextResponse.next();
+  const res = NextResponse.next();
+  res.headers.set("X-Robots-Tag", "noindex, nofollow");
+  return res;
 }
